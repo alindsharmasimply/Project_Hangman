903b5e724fc6187f3614e26925b6d205c3c68021
Alind Sharma <alindsharmasimply@gmail.com>
2017-10-02 05:31:12 +0530

    Starting to get a little idea

--- part1.py
+++ part1.py
@@ -10,7 +10,22 @@ class Hangman():
         self.label.pack()
         self.label.config(font=('Courier', 30, 'bold'))
         self.label.config(foreground='blue', background='black')
-        self.label.config(image=PhotoImage(file='C:\\Users\\S K Sharma\\Desktop\\Project_Hangman\\python_logo.gif'))
+        self.label.config(compound='center')
+
+        self.entry = ttk.Entry(master, width=30)
+        self.entry.pack()
+        self.entry.config(show='*')
+
+        self.button = ttk.Button(master, text="Click Me")
+        self.button.pack()
+        self.logo = PhotoImage(file='C:\\Users\\S K Sharma\\Desktop\\Project_Hangman\\python_logo.gif')
+        self.button.small_logo = self.logo.subsample(5, 5)
+        self.button.config(image=self.button.small_logo, compound='left')
+
+        self.canvas = Canvas(master, width=600, height=400)
+        self.canvas.pack()
+        self.canvas.create_line(50, 50, 20, 20, fill="black", width=5)
+        self.canvas.create_text(100, 25, text="Amazing", font="Helvetica 30 bold")
 
 
 def main():
