9b586fdc899ed87d1b531a07490e38410c0167c8
Alind Sharma <alindsharmasimply@gmail.com>
2017-10-03 21:21:17 +0530

    Just some random inputs

--- part1.py
+++ part1.py
@@ -12,20 +12,27 @@ class Hangman():
         self.label.config(foreground='blue', background='black')
         self.label.config(compound='center')
 
-        self.entry = ttk.Entry(master, width=30)
-        self.entry.pack()
-        self.entry.config(show='*')
+        #self.entry1 = ttk.Entry(master, width=30)
+        # self.entry1.pack()
+        # self.entry1.config(show='*')
 
-        self.button = ttk.Button(master, text="Click Me")
-        self.button.pack()
-        self.logo = PhotoImage(file='C:\\Users\\S K Sharma\\Desktop\\Project_Hangman\\python_logo.gif')
-        self.button.small_logo = self.logo.subsample(5, 5)
-        self.button.config(image=self.button.small_logo, compound='left')
+        self.entry2 = ttk.Entry(master, width = 35)
+        if entry2 in L:
+        	pass
+
+        #self.button = ttk.Button(master, text="Click Me")
+        # self.button.pack()
+        #self.logo = PhotoImage(file='C:\\Users\\S K Sharma\\Desktop\\Project_Hangman\\python_logo.gif')
+        #self.button.small_logo = self.logo.subsample(5, 5)
+        #self.button.config(image=self.button.small_logo, compound='left')
 
         self.canvas = Canvas(master, width=600, height=400)
         self.canvas.pack()
-        self.canvas.create_line(50, 50, 20, 20, fill="black", width=5)
-        self.canvas.create_text(100, 25, text="Amazing", font="Helvetica 30 bold")
+        self.canvas.create_line(188, 115, 168, 80, fill="black", width=5)
+        self.canvas.create_text(300, 100, text="H A N G M A N", font="Helvetica 30 bold")
+
+    def entryProcessing():
+        pass
 
 
 def main():
